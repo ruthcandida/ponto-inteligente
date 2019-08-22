2259106059961df6c470fd9aa25df1abcc47bb9c
Ruth Candida <ruth.sistem@gmail.com>
2019-08-22 15:35:18 -0300

    Initial Project

---
+++ src/main/java/src/ProntoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntoInteligenteApplication.class, args);
+	}
+
+}
