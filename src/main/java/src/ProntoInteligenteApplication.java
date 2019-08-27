ee96069f1f234db603a50bb8e4ee0955c31e58b4
Ruth Candida <ruth.sistem@gmail.com>
2019-08-27 10:41:17 -0300

    feat: Creating CadastroPJController and dependencies

--- src/main/java/src/ProntoInteligenteApplication.java
+++ src/main/java/src/ProntoInteligenteApplication.java
@@ -2,12 +2,21 @@ package src;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class ProntoInteligenteApplication {
-
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class ProntoInteligenteApplication extends SpringBootServletInitializer {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProntoInteligenteApplication.class, args);
+	}
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProntoInteligenteApplication.class);
 	}
 
 }
