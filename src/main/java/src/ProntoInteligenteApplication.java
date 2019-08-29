0d9bad224ec8b933ac65eae101ec492c5b729d0b
Ruth Candida <ruth.sistem@gmail.com>
2019-08-29 11:33:47 -0300

    feat: Add EhCache

--- src/main/java/src/ProntoInteligenteApplication.java
+++ src/main/java/src/ProntoInteligenteApplication.java
@@ -2,11 +2,12 @@ package src;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication() // exclude = { SecurityAutoConfiguration.class }
+@EnableCaching
 public class ProntoInteligenteApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
